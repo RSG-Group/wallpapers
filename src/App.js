2df707b87188bd8860e1e700581147819a3f5c42
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-09-13 21:02:40 +0530

    Fix #2 and modify UI significantly. Colors currently imbalanced.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,34 @@ import WallpaperScreen from './imports/WallpaperScreen'
 
 // Get react-navigation.
 // flow-disable-next-line
-import { DrawerNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 
-export default DrawerNavigator({
+// Get React.
+import React, { Component } from 'react'
+
+const App = TabNavigator({
   Home: { screen: HomeScreen },
   Wallpapers: { screen: WallpaperScreen }
 }, {
   drawerWidth: 250,
   drawerPosition: 'left'
+})
+
+class AppWrapper extends Component {
+  static navigationOptions = {
+    title: 'Not Bad Wallpapers',
+    headerStyle: {
+      backgroundColor: 'teal'
+    }
+  }
+
+  render () {
+    return <App />
+  }
+}
+
+export default StackNavigator({
+  Home: {
+    screen: AppWrapper
+  }
 })
